633667e43c4533a758d775c819fe1e160ff8c044
Andrei Bardier <andrey.bardier@gmail.com>
2024-11-10 00:35:26 +0200

    add preprocess function barebone

--- liniar_regression/car_price_prediction/main.py
+++ liniar_regression/car_price_prediction/main.py
@@ -12,12 +12,29 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 # loading data from .csv into pandas dataframe
 dataset = pd.read_csv('datasets/car details v3.csv')
 
+
 # looking for empty entries
-print(dataset.isnull().sum())
+# print(train_df.isnull().sum())
+
+# remove uncompleted rows (about 230 from total of 7900)
+dataset = dataset.dropna()
+
+
+def preprocess_dataframe(df):
+    df = df.rename(columns={'mileage': 'consumption'})
+    return df
+
+dataset = preprocess_dataframe(dataset)
 
 # dividing features from target variable
 x = dataset.drop(columns=['selling_price'])
 y = dataset['selling_price']
 
 # dividing dataframe to train and test parts
-x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+
+
+print(x_train.columns)
+print(x_test[['year', 'km_driven', 'consumption', 'engine', 'owner']])
